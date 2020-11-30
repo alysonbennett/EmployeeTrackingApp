c3d7bbb6aa2e62d408818a9fb7f52c4a6ba9f7fd
alysonbennett <alysonbennett@gmai.com>
2020-11-30 16:04:55 -0500

    added switch cases

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const consoleTable = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -13,3 +14,46 @@ connection.connect(function(err) {
     if (err) throw err;
     runPrompts();
   });
+
+  function runPrompts() {
+      inquirer.prompt({
+          type: "list",
+          name: "task",
+          message: "What would you like to do?",
+          choices: [
+            "Add department",
+            "Add role",
+            "Add employee",
+            "View employees by department",
+            "View employees by role",
+            "View employee",
+            "Update employee roles",
+            "All done"
+          ]
+      })
+      .then(function(answer) {
+          switch (answer.action) {
+            case "Add department":
+                addDepartment();
+                break
+            case "Add role":
+                addRole();
+                break  
+            case "Add employee":
+                addEmployee();
+                break 
+            case "View employees by department":
+                viewDepartment();
+                break
+            case "View employees by role":
+                viewRole();
+                break
+            case "View employee":
+                viewEmployee();
+                break
+            case "Update employee roles":
+                updateEmployee();
+                break
+          }
+      });
+  }
